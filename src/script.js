35c35ae20ea0877dcc626b253fc4b46355a3cbb3
Adnan2499 <adnanginanjar00@gmail.com>
2026-01-03 10:30:51 +0700

    perubahan style

--- src/script.js
+++ src/script.js
@@ -112,6 +112,17 @@ setTimeout(() => {
 preloader.style.display = "none"; 
 }, 500); 
 }); 
+
+  /* =============================
+     PRELOADER
+  ============================= */
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      preloader.style.opacity = "0";
+      setTimeout(() => (preloader.style.display = "none"), 500);
+    });
+  }
 /* =============================
      LOVE CONFETTI
   ============================= */
